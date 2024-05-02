06febc06eb65b779ac08ab6b1562c4c7307c4949
light <852179641@qq.com>
2024-05-02 20:20:56 +0800

    comment # tcp学习：1.发送消息 2.上传文件

---
+++ src/main/java/com/example/study/tcp/TcpServerDemo01.java
@@ -0,0 +1,64 @@
+package com.example.study.tcp;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TcpServerDemo01 {
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+        InputStream is = null;
+        ByteArrayOutputStream bos = null;
+        try {
+            //1.需要一个服务端地址
+            serverSocket = new ServerSocket(9999);
+            while (true) {
+                // 2.等待客户端建立连接
+                socket = serverSocket.accept();
+                // 3.读取客户端消息 管道流
+                is = socket.getInputStream();
+                byte[] buffer = new byte[1024];
+                int len;
+                bos = new ByteArrayOutputStream();
+                while ((len = is.read(buffer)) != -1) {
+                    bos.write(buffer, 0, len);
+                }
+                System.out.println(bos.toString());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (serverSocket != null) {
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/example/study/tcp/TcpServerDemo02.java
@@ -0,0 +1,37 @@
+package com.example.study.tcp;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TcpServerDemo02 {
+    public static void main(String[] args) throws IOException {
+        // 1.创建服务器地址
+        ServerSocket serverSocket = new ServerSocket(9000);
+        // 2.等待客户端连接
+        Socket socket = serverSocket.accept(); //阻塞式监听，会一直等待客户端连接
+        // 3.获取输入流
+        InputStream is = socket.getInputStream();
+
+        // 4.输出文件
+        FileOutputStream fos = new FileOutputStream("ceShiTcp.md");
+        byte[] buffer = new byte[1024];
+        int len;
+        if ((len = is.read(buffer)) != -1) {
+            fos.write(buffer,0,len);
+        }
+
+        // 5.通知客户端接收完毕
+        OutputStream outputStream = socket.getOutputStream();
+        outputStream.write("我接收完毕了".getBytes());
+
+        outputStream.close();
+        fos.close();
+        is.close();
+        socket.close();
+        serverSocket.close();
+    }
+}
