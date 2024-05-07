413806f0962b3047732f9f534916c6ddd4f8ec07
light <852179641@qq.com>
2024-05-08 00:05:17 +0800

    comment # 多线程学习：线程休眠

---
+++ src/main/java/com/example/study/thread/TestSleep.java
@@ -0,0 +1,33 @@
+package com.example.study.thread;
+
+/**
+ * 模拟网络延时：放大问题的发生性
+ */
+public class TestSleep implements Runnable{
+
+    private int ticketNums = 10;
+
+    @Override
+    public void run() {
+
+        while (true) {
+            if (ticketNums <= 0) {
+                break;
+            }
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(Thread.currentThread().getName() + "--->拿到了第" + ticketNums-- + "张票");
+        }
+    }
+
+    public static void main(String[] args) {
+        TestThreadDemo04 testThreadDemo04 = new TestThreadDemo04();
+
+        new Thread(testThreadDemo04,"张三").start();
+        new Thread(testThreadDemo04,"李四").start();
+        new Thread(testThreadDemo04,"黄牛").start();
+    }
+}
