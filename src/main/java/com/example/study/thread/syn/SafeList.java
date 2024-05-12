de435c9d2aa739855931b47d4c1f6f7d152ab5b3
light <852179641@qq.com>
2024-05-12 23:09:35 +0800

    comment # 死锁

--- src/main/java/com/example/study/thread/syn/SafeList.java
+++ src/main/java/com/example/study/thread/syn/SafeList.java
@@ -1,15 +1,14 @@
 package com.example.study.thread.syn;
 
-import java.util.ArrayList;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 // 测试JUC安全类型的集合
 public class SafeList {
     public static void main(String[] args) throws InterruptedException {
         CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
-       
+
         for (int i = 0; i < 60000; i++) {
-            new Thread(()->{
+            new Thread(() -> {
                 list.add(Thread.currentThread().getName());
             }).start();
         }
