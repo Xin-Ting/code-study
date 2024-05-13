fb2f128dda34dbf84f959bd36099694e8ce188c5
light <852179641@qq.com>
2024-05-13 22:26:19 +0800

    comment # 线程池入门

---
+++ src/main/java/com/example/study/thread/pool/TestThreadPool.java
@@ -0,0 +1,83 @@
+package com.example.study.thread.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TestThreadPool {
+    public static void main(String[] args) {
+        // 创建线程池
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        service.execute(new MyThread());
+        service.execute(new MyThread());
+        service.execute(new MyThread());
+        service.execute(new MyThread());
+        service.shutdown();
+
+    }
+}
+
+class MyThread implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+    }
+}
+
+
+class Demo {
+    int a;
+    int b;
+    int c;
+    int d;
+    int e;
+    int f;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
+
+    public int getE() {
+        return e;
+    }
+
+    public void setE(int e) {
+        this.e = e;
+    }
+
+    public int getF() {
+        return f;
+    }
+
+    public void setF(int f) {
+        this.f = f;
+    }
+}
