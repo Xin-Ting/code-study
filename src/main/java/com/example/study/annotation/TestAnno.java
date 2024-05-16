7a03d65b1a5e75fd6544ff3386a18a34698b203d
light <852179641@qq.com>
2024-05-16 21:54:25 +0800

    comment # 自定义注解

---
+++ src/main/java/com/example/study/annotation/TestAnno.java
@@ -0,0 +1,25 @@
+package com.example.study.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+public class TestAnno {
+    @MyAnnotation(name = "", age = 11, schools = "")
+    public void test() {
+
+    }
+}
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@interface MyAnnotation {
+    // 注解的参数：参数类型+参数名()
+    String name() default "";
+
+    int age();
+
+    String[] schools() default "";
+
+}
