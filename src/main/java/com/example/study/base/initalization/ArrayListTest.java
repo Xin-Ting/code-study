32fd15c2b0447ab2fecb89ebc86ed6793bb3de2a
light <852179641@qq.com>
2025-08-27 23:46:35 +0800

    comment # 测试arraylist

---
+++ src/main/java/com/example/study/base/initalization/ArrayListTest.java
@@ -0,0 +1,23 @@
+package com.example.study.base.initalization;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+
+    private static final Object[] EMPTY = {};
+
+    private transient Object[] elementData;
+
+    public ArrayListTest() {
+        elementData = EMPTY;
+    }
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        ArrayListTest arrayListTest = new ArrayListTest();
+        boolean b = EMPTY == arrayListTest.elementData;
+        System.out.println(b);
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("1");
+    }
+}
